0aec73d78727c57ff46d1176cb9dcf2713220eaa
xu01 <lingyi_xu@hotmail.com>
2014-11-18 13:56:56 +0800

    init

---
+++ XMPP_For_iPhone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XMPP_For_iPhone
+//
+//  Created by xu lingyi on 11/18/14.
+//  Copyright (c) 2014 xuly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
